34c681b1af128181938c2b63f97ac2a4c33bb1d0
nikeetaa231 <nikeetaa231@gmail.com>
2024-02-14 12:43:08 +0530

    fixes

--- src/CardList.js
+++ src/CardList.js
@@ -2,17 +2,19 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-  const cardsArray = robots.map((user, i) => {
-    return (<Card
-      key={i}
-      id={user.id}
-      name={user.name}
-      email={user.email}
-    />);
-  })
   return (
     <div>
-      {cardsArray}
+      {
+        robots.map((user, i) => {
+          return (
+          <Card
+            key={i}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+          />);
+        })
+      }
     </div>
   );
 }
