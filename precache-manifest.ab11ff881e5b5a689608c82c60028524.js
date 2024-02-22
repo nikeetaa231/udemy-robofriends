a42b5ba533ee3da0712bbbbc474e860343030eab
nikeetaa231 <nikeetaa231@gmail.com>
2024-02-22 12:41:07 +0530

    Updates

---
+++ precache-manifest.ab11ff881e5b5a689608c82c60028524.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78782f1d2d87eddfce5305e020fb722",
+    "url": "/udemy-robofriends/index.html"
+  },
+  {
+    "revision": "e6daf001339a35142e01",
+    "url": "/udemy-robofriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "b45cc3eccb1685a476d0",
+    "url": "/udemy-robofriends/static/css/main.77394ca2.chunk.css"
+  },
+  {
+    "revision": "e6daf001339a35142e01",
+    "url": "/udemy-robofriends/static/js/2.36f9ee18.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/udemy-robofriends/static/js/2.36f9ee18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ecc8d4759925795b22",
+    "url": "/udemy-robofriends/static/js/3.7f0ccf67.chunk.js"
+  },
+  {
+    "revision": "b45cc3eccb1685a476d0",
+    "url": "/udemy-robofriends/static/js/main.03de7ed3.chunk.js"
+  },
+  {
+    "revision": "cb17d8ac31d41e8c8d9e",
+    "url": "/udemy-robofriends/static/js/runtime-main.400b4772.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/udemy-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
